0dce01767819391a22593b703bb2d6cb65ea3e74
Marcel Meyer <haithun@gmx.de>
2014-03-06 15:00:23 +0100

    make different settings for local stuff

--- overlord/settings/base.py
+++ overlord/settings/base.py
@@ -52,20 +52,6 @@ ROOT_URLCONF = 'overlord.urls'
 
 WSGI_APPLICATION = 'overlord.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'overlord',
-        'USER': 'overlord',
-        'PASSWORD': 'Lukl9IkxvJ0vexGorh89A8ppGMBfFbaZlRiJAUXQzCMSc1OuDwZGUPiheHdSVlza',
-        'HOST': 'localhost',
-    }
-}
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
